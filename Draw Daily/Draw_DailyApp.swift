6fae55af0213a774c5516b8a05043f34c6529660
Quince Gore-Rodney <spike00890@hotmail.co.uk>
2021-03-22 16:15:29 +0000

    Initial Commit

---
+++ Draw Daily/Draw_DailyApp.swift
@@ -0,0 +1,17 @@
+//
+//  Draw_DailyApp.swift
+//  Draw Daily
+//
+//  Created by New on 22/03/2021.
+//
+
+import SwiftUI
+
+@main
+struct Draw_DailyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
